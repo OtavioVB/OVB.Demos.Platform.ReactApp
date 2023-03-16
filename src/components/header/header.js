df38d2ba2e77730eda80d03deeeaa3ff19e59fb1
OtavioVB <otaviovb.developer@gmail.com>
2023-03-16 18:28:25 -0300

    feat: `account login and creation`

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -28,7 +28,7 @@ function Header() {
     return (
         <header>
             <div class="description-platform">
-                <p class="NamePlatform">Eschody</p>
+                <p class="NamePlatform">Olizia</p>
             </div>
             <div class="navigation-area">
                 <nav class="navigation-area-content">
@@ -36,7 +36,7 @@ function Header() {
                         <li class="navigation-item">
                             <p id="support-click" class="navigation-link" href="https://google.com.br">Suporte <svg id="support-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16"><path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/></svg></p>
                             <div class="navigation-area-content-subgroups-content" id="navigation-area-content-subgroup-identifier">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="close" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" id="close" class="bi bi-x" viewBox="0 0 16 16"><path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/></svg>
                                 <ul class="navigation-area-content-subgroup">
                                     <li class="navigation-item-subgroup">Ajuda</li>
                                     <li class="navigation-item-subgroup">Contato</li>
@@ -45,6 +45,14 @@ function Header() {
                             </div>
                         </li>
                     </ul>
+                    <div class="account-area-content">
+                        <p class="account-area-content-text">ACESSO</p>
+                        <div class="navigation-area-content-account-subgroup">
+                            <p>Acesso</p>
+                            <p class="navigation-area-content-account-subgroup-item">ENTRAR COMO MEMBRO</p>
+                            <p class="navigation-area-content-account-subgroup-item">ENTRAR COMO ADMINISTRADOR</p>
+                        </div>
+                    </div>
                 </nav>
             </div>
         </header>
--- src/components/main/home/landingpage.js
+++ src/components/main/home/landingpage.js
@@ -3,14 +3,7 @@ import './landingpage.css';
 function LandingPage(){
     return (
         <main class="landingpage">
-            <section class="main-section">
-                <div class="main-section-content">
-                    <h1>O que é o Eschody?</h1>
-                    <div class="main-section-content-display" id="messages">
-                        <p class="main-section-context-display-text">O <b>Eschody</b> é uma plataforma criada por estudantes que estão em processo de estudos para o vestibular para estudantes que estão ou irão estudar para os diversos vestibulares pelo pais. A plataforma tem o dever de auxiliar o aluno em análises de lacunas no conhecimento, em análise do nível de comprometimento e análise do atual estado perante o resultado esperado.</p>
-                    </div>
-                </div>
-            </section>
+
         </main>
     );
 }
