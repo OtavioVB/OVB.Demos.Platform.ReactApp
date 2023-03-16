b8e7f2717b1be658e6528ac104e496c8980e88cb
OtavioVB <otaviovb.developer@gmail.com>
2023-03-15 22:05:46 -0300

    feat: ´menu dropdown

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,23 @@
 import './header.css';
+import React from 'react';
 
 function Header() {
+    React.useEffect(() => {
+        let isSetted = false;
+
+        document.getElementById("support-click").addEventListener("click", () => {
+            const subgroup = document.getElementById("navigation-area-content-subgroup-identifier");
+
+            if(isSetted === true){
+                subgroup.style = "display: none;"
+                isSetted = false;
+            }else{
+                subgroup.style = "display: flex;"
+                isSetted = true;
+            }
+        });
+    });
+
     return (
         <header>
             <div class="description-platform">
@@ -9,7 +26,13 @@ function Header() {
             <div class="navigation-area">
                 <nav class="navigation-area-content">
                     <ul class="navigation-area-content-group">
-                        <li class="navigation-item"><a class="navigation-link" href="https://google.com.br">Suporte</a></li>
+                        <li class="navigation-item">
+                            <p id="support-click" class="navigation-link" href="https://google.com.br">Suporte</p>
+                            <ul id="navigation-area-content-subgroup-identifier" class="navigation-area-content-subgroup">
+                                <li class="navigation-item-subgroup">Ajuda</li>
+                                <li class="navigation-item-subgroup">Desenvolvedor</li>
+                            </ul>
+                        </li>
                     </ul>
                 </nav>
             </div>
